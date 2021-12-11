b98d92ed14f26e7cf7463123f045d154aa691347
EugeneRaven <71719125+EugeneRaven@users.noreply.github.com>
2021-12-11 20:03:01 +0300

    Add files via upload
    Вместе с обратной.

--- Task11.py
+++ Task11.py
@@ -1,5 +1,6 @@
 #from numbers import number - не устанавливается numbers.
 # Надеюсь, группа тоже подойдёт.
+
 def table(a,b,c):
     #if isinstance(a, float) and isinstance(b, float) and isinstance(c, float):
     return [[1,a,b], [0,1,c], [0,0,1]]
@@ -10,6 +11,8 @@ def multiplication(x,y):
     b = x[0][2] + y[0][2] + x[0][1]*y[1][2]
     c = x[1][2] + y[1][2]
     return table(a,b,c)
+def inverse(x):
+    return [[1,-x[0][1],(x[0][1]*x[1][2]-x[0][2])], [0,1,-x[1][2]], [0,0,1]]
 a1 = int(input())
 b1 = int(input())
 c1 = int(input())
@@ -18,6 +21,15 @@ b2 = int(input())
 c2 = int(input())
 x = table(a1,b1,c1)
 y = table(a2,b2,c2)
+inv1 = inverse(x)
+inv2 = inverse(y)
+print("Обратная к первой:")
+for i in range(3):
+    print(*inv1[i])
 g = multiplication(x,y)
+print("Обратная ко второй:")
+for i in range(3):
+    print(*inv2[i])
+print("Произведение:")
 for i in range(3):
     print(*g[i])
