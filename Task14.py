09f4bee6c9cab735e0295424befc0060977249f2
EugeneRaven <71719125+EugeneRaven@users.noreply.github.com>
2021-12-18 21:32:35 +0300

    Add files via upload

--- Task14.py
+++ Task14.py
@@ -1,10 +1,14 @@
 import math
+def p(a):
+    count = 0
+    check = False
+    for i in range (2, (math.ceil(math.sqrt(a))+1)):
+        if a%i == 0:
+            count = 1
+    if (count == 1):
+        check = False
+    else:
+        check = True
+    return check
 a = int(input())
-count = 0
-for i in range (2, (math.ceil(math.sqrt(a))+1)):
-    if a%i == 0:
-        count = 1
-if (count == 1):
-    print("No")
-else:
-    print("Yes")
+print(p(a))
